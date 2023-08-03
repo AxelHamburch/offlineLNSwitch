911f888e4937aae1c0bbce1b8a47e6e946d91c6e
axelhamburch <axelhamburch@protonmail.com>
2023-08-03 20:32:25 +0200

    first update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 
-#define LED_PIN 12
-#define BLINK_DELAY 100
+#define LED_PIN 21
+#define BLINK_DELAY 250
 
-int n = 0
+int n = 0;
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("# Start #");
   pinMode(LED_PIN, OUTPUT);
 }
 
@@ -16,7 +17,7 @@ void loop() {
   digitalWrite(LED_PIN, LOW);
   delay(BLINK_DELAY);
 
-  Serial.println(sin(n * 2 * 3.1415 / 20));
-  n++;
+  // Serial.println(sin(n * 2 * 3.1415 / 20));
+  // n++;
 }
 
